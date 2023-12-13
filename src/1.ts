fee58717989c1c83ae8fdc54255f731fb96568b4
iVladyuser <korol.v09@gmail.com>
2023-12-13 13:18:42 +0100

    update 1.ts

--- src/1.ts
+++ src/1.ts
@@ -4,38 +4,8 @@
   напишіть скорочену ініціалізацію.
 */
 
-// class Student {
-// 	constructor(name: string, age: number, grade: string) {}
-// }
+class Student {
+	constructor(name: string, age: number, grade: string) {}
+}
 
-// export {};
-
-// abstract class Plane {
-//   protected pilotInCabin = false;
-
-//   public sitInPlane() {
-//     this.pilotInCabin = true;
-//   }
-
-//   public abstract startEngine(): boolean;
-// }
-
-// class Maize extends Plane {
-//   public startEngine() {
-//     // Запускаємо винти двигуна.
-
-//     return true;
-//   }
-// }
-
-// class Boeing extends Plane {
-//   public startEngine() {
-//     // Розігріваємо реактивні турбіни.
-
-//     return true;
-//   }
-// }
-
-// class BrokenPlane extends Plane {} // Error: Клас 'BrokenPlane' не реалізовує унаслідований абстрактний метод 'startEngine' з класу 'Plane'.
-
-// export {};
+export {};
